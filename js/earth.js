9bfd8aae1aa90f858c4c187f451a6e8596a8e9b2
MazeBuer <mazikeenbuer@gmail.com>
2022-07-28 18:15:45 -0700

    changed earth to astronauts panel

---
+++ js/earth.js
@@ -0,0 +1,49 @@
+const panels = document.querySelectorAll('.panel')
+
+function toggleOpen() {
+    this.classList.toggle('open')
+}
+
+//need to add if statement because there are mulitple transition events (text increase and flex-grow - panel increaese). So if property includes flex we will toggle it
+
+function toggleActive(e) {
+    if(e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active')
+    }
+}
+
+panels.forEach(panel => panel.addEventListener('click',toggleOpen)) //to expand each panel on click
+panels.forEach(panel => panel.addEventListener('transitionend',toggleActive))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let key = 'uuLAFR0G9uDUaF3dKdt2fmoDI6khzX626ypaP5md'
